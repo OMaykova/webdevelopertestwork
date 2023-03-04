26fa3be501823ae9fd8179806e11fc825abd2100
Olga Maykova <dolkaaaaa@gmail.com>
2023-03-04 19:51:07 +0300

    feat csv

--- src/Components/Table/Table.tsx
+++ src/Components/Table/Table.tsx
@@ -20,26 +20,72 @@ function Table() {
         <td className='order-item' >{Instrument[item.instrument]}</td>
       </tr>
     )
-    
   }
+  function tableToCSV() {
+    let arr: any = [];
+    listOfOrders.forEach((element: { [s: string]: unknown; } | ArrayLike<unknown>) => {
+      arr.push(Object.values(element))
+    })
+    arr.map((i:any) => {
+      i[3]= OrderStatus[i[3]]
+      i[4]= OrderSide[i[4]]
+      i[7]= Instrument[i[7]]
+    })
+    let arr2: any[] =['Id,Creation time,Change time,Status,Side,Price,Amount,Instrument']
+    arr.map((e: { toString: () => any; }) => {
+      arr2.push(e.toString())
+    })
+    let csv_data: string
+    
+    csv_data = arr2.join('\n');
+    
+    downloadCSVFile(csv_data);
+
+}
+  function downloadCSVFile(csv_data:any) {
+
+    let CSVFile = new Blob([csv_data], { type: "text/csv" });
+    let temp_link = document.createElement('a');
+    temp_link.download = "orders.csv";
+    let url = window.URL.createObjectURL(CSVFile);
+    temp_link.href = url;
+    temp_link.style.display = "none";
+    document.body.appendChild(temp_link);
+    temp_link.click();
+    document.body.removeChild(temp_link);
+  }
+      
   return (
-		<table className='table'>
-      <thead className='table-title'>
-        <tr className='title'>
-          <th className='order-item'>Id</th>
-          <th className='order-item'>Creation time</th>
-          <th className='order-item'>Change time</th>
-          <th className='order-item'>Status</th>
-          <th className='order-item'>Side</th>
-          <th className='order-item'>Price</th>
-          <th className='order-item'>Amount</th>
-          <th className='order-item'>Instrument</th>
-        </tr>
-      </thead>
-      <tbody>
-        {listOfOrders.map((el: any, index: any) => renderListItems(el, index))}
-      </tbody>
-    </table>
+    <>
+      <table className='table'>
+        <thead className='table-title'>
+          <tr className='title'>
+            <th className='order-item'>Id</th>
+            <th className='order-item'>Creation time</th>
+            <th className='order-item'>Change time</th>
+            <th className='order-item'>Status</th>
+            <th className='order-item'>Side</th>
+            <th className='order-item'>Price</th>
+            <th className='order-item'>Amount</th>
+            <th className='order-item'>Instrument</th>
+          </tr>
+        </thead>
+        <tbody>
+          {listOfOrders.map((el: any, index: any) => renderListItems(el, index))}
+        </tbody>
+      </table>
+      {listOfOrders.length > 0 ? 
+        <button className='table-btnCSV' onClick={() => tableToCSV()}>
+          Выгрузка таблицы в .csv
+        </button>
+        : 
+        <button
+          disabled
+          className='table-btnCSV table-btnCSV_disabled'>
+          Выгрузка таблицы в .csv
+        </button>
+      }
+    </>
   )
 }
 
